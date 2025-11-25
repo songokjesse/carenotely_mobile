5d5e630e12ad214a9e950ecb5cb12544130150b6
Jesse Songok <jessesongok@gmail.com>
2025-11-25 13:38:32 +0800

    feat: add CareNotely title to login screen and configure stack screens in root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen name="signup" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
